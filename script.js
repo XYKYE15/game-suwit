f29c7003b6a18597351163516b752ef7ce4230fe
Rizki Rinaldi <rizkirenaldi115@gmail.com>
2026-09-11 15:02:27 +0700

    update animasi putar pada pilihan computer

--- script.js
+++ script.js
@@ -13,6 +13,21 @@ function getHasil(comp, player) {
   if (player == "kelingking") return comp == "jempol" ? "MENANG" : "KALAH";
 }
 
+function putar() {
+  const imgComputer = document.getElementsByClassName("imgComputer")[0];
+  const gambar = ["jempol", "telunjuk", "kelingking"];
+  let i = 0;
+  const waktuMulai = new Date().getTime();
+  setInterval(function () {
+    if (new Date().getTime() - waktuMulai > 1000) {
+      clearInterval;
+      return;
+    }
+    imgComputer.setAttribute("src", `img/${gambar[i++]}.png`);
+    if (i == gambar.length) return (i = 0);
+  }, 100);
+}
+
 const pilihanImg = document.querySelectorAll(".imgPlayer img");
 pilihanImg.forEach(function (pilihan) {
   pilihan.addEventListener("click", function () {
@@ -20,20 +35,23 @@ pilihanImg.forEach(function (pilihan) {
     const pilihanPlayer = pilihan.className;
     const hasil = getHasil(pilihanComp, pilihanPlayer);
 
-    const imgComputer = document.getElementsByClassName("imgComputer")[0];
-    imgComputer.setAttribute("src", `img/${pilihanComp}.png`);
+    putar();
 
-    const info = document.getElementsByClassName("info")[0];
-    info.innerHTML = hasil;
+    setTimeout(function () {
+      const imgComputer = document.getElementsByClassName("imgComputer")[0];
+      imgComputer.setAttribute("src", `img/${pilihanComp}.png`);
 
-    // menentukan score
-    let totalWin = document.getElementById("totalWin");
-    let totalLose = document.getElementById("totalLose");
-    if (hasil == "MENANG") {
-      totalWin.innerHTML = parseInt(totalWin.innerHTML) + 1;
-    } else if (hasil == "KALAH") {
-      totalLose.innerHTML = parseInt(totalLose.innerHTML) + 1;
-    }
+      const info = document.getElementsByClassName("info")[0];
+      info.innerHTML = hasil;
+
+      // menentukan score
+      let totalWin = document.getElementById("totalWin");
+      let totalLose = document.getElementById("totalLose");
+      if (hasil == "MENANG") {
+        totalWin.innerHTML = parseInt(totalWin.innerHTML) + 1;
+      } else if (hasil == "KALAH") {
+        totalLose.innerHTML = parseInt(totalLose.innerHTML) + 1;
+      }
+    }, 1000);
   });
 });
-
